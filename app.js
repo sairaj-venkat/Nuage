d505c65472cc129483fb4a16bcc7988b03c81b11
sairaj <svvuriti@ncsu.edu>
2016-03-18 22:54:20 -0400

    Web App

---
+++ app.js
@@ -0,0 +1,12 @@
+// This will create a server and calls the index.html
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+	res.end();
+    });
+}).listen(8000, '127.0.0.1');
