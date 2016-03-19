1cbf9fe3809b3bf84d57af191f864262faf68879
sairaj <svvuriti@ncsu.edu>
2016-03-18 23:29:45 -0400

    Script logging added

--- interact.py
+++ interact.py
@@ -1,5 +1,7 @@
 import requests
+import logging
 
+logging.basicConfig(filename='scriptlog.log', level=logging.INFO)
 print '\n';
 print '*********Welcome to the Nuage Test App**********';
 print '*****Enter 1 to get the url of the App*****';
@@ -10,6 +12,7 @@ print '***********Enter 5 to quit****************';
 
 url = 'http://localhost'
 option = input('Enter an integer between 1 and 5 \t');
+logging.info('Option entered \t' + str(option))
 
 while (option != 5):
 
@@ -19,16 +22,18 @@ while (option != 5):
 	else:
 		nid = input('\nEnter the id \t');
 		nname = input('Enter the name within quotes \t');
-		body = {'id': nid, 'name': nname}
+		body = {'pid': nid, 'name': nname}
 
 	if option == 2:		
-		requests.post(url, data=body)		
+		requests.post(url, data=body)	
+		logging.info('Posted: \t' + 'id ' + str(body['pid']) + 'Name is ' + body['name'])	
 		
 	if option == 3:		
 		requests.put(url, data=body)
 
 	if option == 4:		
 		requests.delete(url, data=body)
+		logging.info('Deleted: \t' + 'id ' + str(body['pid']) + 'Name is ' + body['name'])	
 
 
 	print '\n';
@@ -38,5 +43,6 @@ while (option != 5):
 	print '*****Enter 4 to delete the data from the App*****';
 	print '***********Enter 5 to quit****************';
 	option = input('Enter an integer between 1 and 5 \t');
+	logging.info('Next entered \t' + str(option))
 
 
